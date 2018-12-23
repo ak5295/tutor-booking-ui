1fb18b47498870d84f66c364033f30008726ff50
Lillian Lee <lillianlee62@gmail.com>
2018-12-23 13:25:58 -0500

    add tutors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import {HomeComponent} from './home/home.component';
 import {Routes, RouterModule} from '@angular/router';
+import { TutorsComponent } from './tutors/tutors.component';
 // import: [ RouterModule.forRoot(routes)];
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'tutors', component: TutorsComponent}
 ];
 
 @NgModule({
---
+++ src/app/tutors/tutors.component.ts
@@ -0,0 +1,20 @@
+import { ApiService } from './../service/api.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tutors',
+  templateUrl: './tutors.component.html',
+  styleUrls: ['./tutors.component.scss']
+})
+export class TutorsComponent implements OnInit {
+  tutors: any[];
+
+  constructor(private api: ApiService ) { }
+
+  ngOnInit() {
+    this.api.getTutors().subscribe(result => {
+      this.tutors = result;
+    });
+  }
+
+}
